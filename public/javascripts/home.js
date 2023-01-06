9dc128d83533e7ce4ccb9f589aad27be9393e831
Jonnata Costa <jonnatacosta@hotmail.com>
2023-01-06 13:04:07 -0300

    add function decryptText

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -3,7 +3,13 @@ var finalText
 var finalTextId
 
 function encryptText() {
-    var textArea = document.getElementById("textbox").value;
-    finalText = textArea.replace("e", "enter").replace("i", "imes").replace("a", "ai").replace("o", "ober").replace("u", "ufat");
+    var inputText = document.getElementById("textbox").value;
+    finalText = inputText.replace("e", "enter").replace("i", "imes").replace("a", "ai").replace("o", "ober").replace("u", "ufat");
     document.getElementById("result").innerHTML = finalText;
+}
+
+function decryptText() {
+    var inputText = document.getElementById("textbox").value;
+    finalTextId = inputText.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
+    document.getElementById("result").innerHTML = finalTextId;
 }
