d60ca2e3dc21101e668e84d1c873b0b7fb795391
Jonnata Costa <jonnatacosta@hotmail.com>
2023-01-06 22:02:11 -0300

    Adjustments in the copy function, add dark mode...

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -13,3 +13,27 @@ function decryptText() {
     finalTextId = inputText.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
     document.getElementById("result").innerHTML = finalTextId;
 }
+
+function copyText() {
+    var copyText = document.getElementById("result").innerHTML;
+    navigator.clipboard.writeText(copyText);
+    alert("You just copy this: " + copyText);
+}
+
+function clearText() {
+    document.getElementById("textbox").value = "";
+    document.getElementById("result").innerHTML = "";
+}
+
+/*DARK MODE*/
+const toggleSwitch = document.querySelector('.toggle-button');
+
+function switchTheme(e) {
+  if(e.target.checked) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+  }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
